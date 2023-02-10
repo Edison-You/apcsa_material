c5c7ea21c842f2590697f5cd3555a7af6e2f9610
Edison-You <124083906+Edison-You@users.noreply.github.com>
2023-02-10 13:19:13 -0500

    Update StudyAtHome.java

--- classwork/2023-02-10/StudyAtHome.java
+++ classwork/2023-02-10/StudyAtHome.java
@@ -1,30 +1,40 @@
 import java.util.*;
+import java.util.Arrayllist 
 
 public class StudyAtHome
 {
    public static void main(String[] args)
    {
        // Rewrite this code to use an ArrayList instead of an array
-       String[] toDoList = new String[3];
-       toDoList[0] = "Review the lessons";
-       toDoList[1] = "Drink water";
-       toDoList[2] = "Work on the lab";
-
+//        String[] toDoList = new String[3];
+//        toDoList[0] = "Review the lessons";
+//        toDoList[1] = "Drink water";
+//        toDoList[2] = "Work on the lab";
+         ArrayList<String> toDoList = new ArrayList<String>(); 
+         listStrings.add("Review the lessons");
+         listStrings.add("Drink water"); 
+         listStrings.add("Work on the lab"); 
+      System.out.println(toDoList);
+         
        // changing element 1
-       toDoList[1] = "Do the homeworks";
+         listStrings.set(1, "Do the Homeworks");
+         System.out.println(toDoList) 
+//        toDoList[1] = "Do the homeworks";
 
-       System.out.println(toDoList.length + " things to do!");
+       System.out.println(toDoList.size() + " things to do!");
        System.out.println("Here's the first thing to do: "
-           + toDoList[0] );
+           + toDoList.get(1));
 
        // remove item 0 and move everything down
        //  (this can be done in 1 command with ArrayList)
-       toDoList[0] = toDoList[1];
-       toDoList[1] = toDoList[2];
-       toDoList[2] = "";
-
-       System.out.println("Here's the next thing to do: "
-           + toDoList[0] );
+      toDoList.remove(0);
+      
+//        toDoList[0] = toDoList[1];
+//        toDoList[1] = toDoList[2];
+//        toDoList[2] = "";
+           System.out.println("Here's the next thing to do: " + toDoList.get(0))
+//        System.out.println("Here's the next thing to do: "
+//            + toDoList[0] );
 
    }
 }
